65809c1911195ec71a3ab6bbede958646de32fd1
iakdere1 <123518771+iakdere1@users.noreply.github.com>
2023-09-10 22:47:20 -0400

    Add files via upload

---
+++ Generic Lab/DataSetGen.java
@@ -0,0 +1,38 @@
+package work;
+
+public class DataSetGen <T>{
+	
+	private double sum;
+	private Measurable maximum;
+	private int count;
+	
+	public DataSetGen()
+	{
+		sum = 0;
+		count = 0;
+		maximum = null;
+	}
+	
+	public void add(Measurable x)
+	{
+		sum = sum + x.getMeasure();
+	
+	if (count == 0 || maximum.getMeasure() < x.getMeasure())
+	
+		maximum = x;
+	
+	count++;
+}
+	
+	public double getAverage()
+	{
+		if (count == 0) return 0;
+		else return sum / count;
+	}
+	
+	public T getMaximum()
+	{
+		return (T) maximum;
+	}
+
+}
