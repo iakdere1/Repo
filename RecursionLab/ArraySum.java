d94e97b6a5e3d54c69156606f5ac2b6b220a2213
iakdere1 <123518771+iakdere1@users.noreply.github.com>
2023-10-01 20:55:31 -0400

    Add files via upload

---
+++ RecursionLab/ArraySum.java
@@ -0,0 +1,16 @@
+package work;
+
+public class ArraySum {
+	
+	public int sumOfArray(Integer[] a, int index)
+	{
+		
+		if (index < 0)
+		{
+			return 0;
+		}
+		return a[index] + sumOfArray(a,index - 1);
+		
+	}
+
+}
